05efcd3c787f4f38d4497d62f838ced66e693685
Amnisia <nxexox@gmail.com>
2018-05-04 16:47:52 +0500

    mini fix

--- drf_auto/__init__.py
+++ drf_auto/__init__.py
@@ -1,3 +1,20 @@
-VERSION = (0, 4, "1a0")
-__version__ = ".".join(map(str, VERSION))
+r"""
+______ ______ ______         ____
+| ___ \| ___ \   __|        /    \     __  __ _____________
+| |  \ \ |_/ /  |__        /  __  \   | |  | |__   __|  _  \
+| |  | |    /|   __| ---- /  /__\  \  | |  | |  | |  | / \ |
+| |_/ /| |\ \|  |        /  ______  \ | |__| |  | |  | \_/ |
+|____/ \_| \_\__|       /__/      \__\|______|  |_|  \____/
+
+"""
+
+VERSION = (0, 4, '1a0')
 default_app_config = "drf_auto.apps.DRFAutoConfig"
+
+__title__ = 'Django REST framework Auto'
+__author__ = 'Deys Timofey'
+__email__ = 'nxexox@gmail.com'
+__copyright__ = 'Copyright (c) 2018 Deys Timofey'
+__license__ = 'Apache License 2.0'
+__url__ = 'https://github.com/nxexox/django-rest-framework-auto'
+__version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 import drf_auto
 import os
-from setuptools import setup
+
+from setuptools import setup, find_packages
+
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 
 setup(
-    name='drf-auto',
+    name=drf_auto.__title__,
     version=drf_auto.__version__,
-    packages=['drf_auto'],
+    packages=find_packages(exclude=('tests', 'docs')),
     license='Apache 2.0',  # Ставим лицензию
     description='Auto docs, auto tests, auto helpers for django rest framework. Python3.x, Django>=1.9',
     long_description=README,
-    author='Deys Timofey',
-    author_email='nxexox@gmail.com',
+    url=drf_auto.__url__,
+    author=drf_auto.__author__,
+    author_email=drf_auto.__email__,
+    maintainer=drf_auto.__author__,
+    maintainer_email=drf_auto.__email__,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
